6324ca7c361731b55126d07e74e71810a4402a3f
코딩하는 춘배 <kaan03@naver.com>
2024-01-10 20:32:21 +0900

    24.01.10

--- src/_1931.java
+++ src/_1931.java
@@ -3,28 +3,52 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
+/*
+ * num = { {1, 2}, {1, 4}, {2, 3} ... };
+ */
 public class _1931 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
 		int N = Integer.parseInt(br.readLine());
-		StringTokenizer st;
-		int count = 0; // 회의 수
-		int preEndT = 0;
-		int startT, endT;
+		int[][] num = new int[N][2];
+
 		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			startT = Integer.parseInt(st.nextToken());
-			endT = Integer.parseInt(st.nextToken());
-			if (preEndT <= startT) {
-				count ++;
-				preEndT = endT;
-			}
-			else continue;
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			num[i][0] = Integer.parseInt(st.nextToken());
+			num[i][1] = Integer.parseInt(st.nextToken());
 		}
+		
+		Arrays.sort(num, (e1, e2) -> {
+			// 끝나는 시간이 같다면
+			if (e1[1] == e2[1]) {
+				// 시작하는 시간 순으로 정렬
+				if (e1[0] >= e2[0]) return 1;
+				else return -1;
+			}
+			// 끝나는 시간이 다르다면
+			else {
+				if (e1[1] > e2[1]) return 1;
+				else return -1;
+			}
+		});
+
+		int count = 0;
+		int endT = 0;
+		
+		for (int[] arr: num) {
+			// 해당 idx의 시작하는 시간이 마지막 회의 종료 시간보다 같거나 나중이라면
+			if (arr[0] >= endT) {
+				count ++;
+				endT = arr[1];
+			}
+		}
+		
 		bw.write(count + "");
 		bw.flush();
 		bw.close();
