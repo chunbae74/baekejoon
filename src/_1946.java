0ef71813f20ea0d39f6ba3d9a96e5fdb0b4e1ac8
코딩하는 춘배 <kaan03@naver.com>
2024-01-18 07:17:20 +0900

    24.01.18

--- src/_1946.java
+++ src/_1946.java
@@ -15,7 +15,7 @@ public class _1946 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int T = Integer.parseInt(br.readLine());
+		int T = Integer.parseInt(br.readLine()); 
 		for (int i = 0; i < T; i++) {
 			int N = Integer.parseInt(br.readLine());
 			int[][] arr = new int[N][2];
