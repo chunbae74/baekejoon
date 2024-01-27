7ad9a7aa9a6d2dd5cb73e6f412c582aaef4fea81
코딩하는 춘배 <kaan03@naver.com>
2024-01-27 22:07:57 +0900

    24.01.27

--- src/_1748.java
+++ src/_1748.java
@@ -9,10 +9,16 @@ public class _1748 {
    public static void main(String[] args) throws IOException {
 	   BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	   BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	   long N = Integer.parseInt(br.readLine());
-	   long count = 0;
-	   for (long i = 1; i <= N; i++) {
-		   count += Long.toString(i).length();
+	   int N = Integer.parseInt(br.readLine());
+	   int count = 0;
+	   int len = 1;
+	   int 기준 = 10;
+	   for (int i = 1; i <= N; i++) {
+		   if (i % 기준 == 0) {
+			   기준 *= 10;
+			   len ++;
+		   }
+		   count += len;
 	   }
 	   bw.write(count + "");
 	   bw.flush();
