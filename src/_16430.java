5a8e546976ad741d694de9494d433fb2e3214ebf
chunbae74 <kaan03@naver.com>
2024-03-26 23:47:28 +0900

    24.03.26

---
+++ src/_16430.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class _16430 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+	
+		bw.write((B - A) + " " + B);
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
