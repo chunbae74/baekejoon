491ede3a9c98a01627abf792678235ca26f52ac1
chunbae74 <kaan03@naver.com>
2024-04-05 22:52:57 +0900

    24.04.05

---
+++ src/_1809.java
@@ -0,0 +1,13 @@
+
+public class _1809 {
+
+	public static void main(String[] args) {
+		System.out.println("(___)");
+		System.out.println("(o o)____/");
+		System.out.println(" @@      \\");
+		System.out.println("  \\ ____,/");
+		System.out.println("  //   //");
+		System.out.println(" ^^   ^^");
+	}
+
+}
