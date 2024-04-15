227c0b8855a9f09aca06a19fdb9d607698aaf8f0
chunbae74 <kaan03@naver.com>
2024-04-15 23:08:54 +0900

    24.04.15

--- src/_1406.java
+++ src/_1406.java
@@ -3,40 +3,45 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class _1406 {
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		Stack<Character> stack = new Stack<>();
+		char[] arr = new char[600_000];
+
 		String input = br.readLine();
-		for(Character c: input.toCharArray()) stack.add(c);
+		int idx = 0;
+		for (; idx < input.length(); idx++) {
+			arr[idx] = input.charAt(idx);
+		}
 		
+		// 글자 개수
+		int len = idx;
+		// 입력할 명령어의 개수
 		int M = Integer.parseInt(br.readLine());
 		
-		Character command; 
-		Character text = '\u2000';
-		int index = input.length();
-		
 		for(int i = 0; i < M; i++) {
-			String s = br.readLine();
-			command = s.charAt(0);
-			
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			char command = st.nextToken().charAt(0);
+
 			switch(command) {
 			case 'L' : // 왼쪽으로
-				if (index != 0) index --;
+				if (idx >= 0) idx --;
 				break;
 				
 			case 'D': // 오른쪽으로
-				if (index != stack.size()) index ++;
+				if (idx < len) idx ++;
 				break;
 				
 			case 'B': // 왼쪽문자 삭제
-				if (index == 0) break;
-				stack.remove(--index);
+				if (idx < 0) break;
+				
+				for (int j = idx; j < len - 1; j++) {
+					arr[j] = arr[j + 1];
+				}
+				len --;
 				break;
 				
 			case 'P': // 문자 왼쪽에 추가
---
+++ src/_16395.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class _16395 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		Integer[][] dp = new Integer[N][];
+		
+		dp[0] = new Integer[] { 1 };
+		dp[1] = new Integer[] { 1, 1 };
+		for (int i = 2; i < N; i++) {
+			dp[i] = new Integer[i + 1];
+			for (int j = 0; j < (i + 1); j++) {
+				if (j == 0 || j == i) dp[i][j] = 1;
+				else dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+			}
+		}
+		
+		bw.write(dp[N - 1][K - 1] + "");
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
