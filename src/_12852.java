bf1cdb27f6de6796f04b7aed10cade700d88b7de
chunbae74 <kaan03@naver.com>
2024-04-23 21:28:45 +0900

    24.04.23

---
+++ src/_12852.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class _12852 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		boolean isDebug = false;
+		int N = Integer.parseInt(br.readLine());
+		Integer[] dp = new Integer[N + 1];
+		int[] idxArr = new int[N + 1];
+		
+		dp[0] = dp[1] = 0;
+		idxArr[1] = 0;
+		
+		if (N > 1) {
+			dp[2] = 1;
+			idxArr[2] = 1;
+		}
+		if (N > 2) {
+			dp[3] = 1;
+			idxArr[3] = 1;
+		}
+		
+		for (int i = 4; i <= N; i++) {
+			if ((i % 3) == 0) {
+				if (dp[i / 3] < dp[i - 1]) {
+					dp[i] = dp[i / 3] +  1;
+					idxArr[i] = i / 3;
+				} else {
+					dp[i] = dp[i - 1] + 1;
+					idxArr[i] = i - 1;
+				}
+			}
+			if ((i % 2) == 0) {
+				if (dp[i / 2] < dp[i - 1]) {
+					dp[i] = dp[i / 2] + 1;
+					idxArr[i] = i / 2;
+				} else {
+					dp[i] = dp[i - 1] + 1;
+					idxArr[i] = i - 1;
+				}
+			}
+			if ((i % 3) != 0 && (i % 2) != 0) {
+				dp[i] = dp[i - 1] + 1;
+				idxArr[i] = i - 1;
+			}
+		}
+		
+		if (isDebug) {
+			System.out.println(Arrays.toString(dp));
+			System.out.println(Arrays.toString(idxArr));
+		}
+		
+		sb.append(dp[N]).append("\n");
+		
+		sb.append(N).append(" ");
+		int targetIdx = idxArr[N];
+		while (targetIdx > 0) {
+			sb.append(targetIdx).append(" ");
+			targetIdx = idxArr[targetIdx];
+		}
+		
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
