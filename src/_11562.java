d66aa591f5b560d953c719c4bfc6abc4c03701c0
chunbae74 <kaan03@naver.com>
2024-05-23 21:08:01 +0900

    24.05.23

--- src/_11562.java
+++ src/_11562.java
@@ -5,6 +5,9 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
+/*
+ * 아이디어: https://www.acmicpc.net/board/view/137044
+ */
 public class _11562 {
 
 	public static void main(String[] args) throws IOException {
@@ -16,9 +19,7 @@ public class _11562 {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
-		
 		int[][] dist = new int[N][N];
-		boolean[][] isOneWay = new boolean[N][N];
 		
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
@@ -27,7 +28,7 @@ public class _11562 {
 			}
 		}
 		
-		for (int i = 0; i < N; i++) {
+		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int U = Integer.parseInt(st.nextToken()) - 1;
 			int V = Integer.parseInt(st.nextToken()) - 1;
@@ -35,18 +36,35 @@ public class _11562 {
 			// b = 0: 일방통행 u -> v
 			// b = 1: 양방향 길
 			
-			if (B == 0) isOneWay[U][V] = true;
-			dist[U][V] = dist[V][U] = 1;
+			if (B == 0) {
+				dist[U][V] = 0;
+				dist[V][U] = 1;
+			} else {
+				dist[U][V] = dist[V][U] = 0;
+			}
 		}
 		
 		for (int k = 0; k < N; k++) {
 			for (int i = 0; i < N; i++) {
 				for (int j = 0; j < N; j++) {
 					if (i == j) continue;
-					else dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][i]);
+					dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
 				}
 			}
 		}
+		
+		int K = Integer.parseInt(br.readLine());
+		for (int k = 0; k < K; k++) {
+			st = new StringTokenizer(br.readLine());
+			int start = Integer.parseInt(st.nextToken()) - 1;
+			int end = Integer.parseInt(st.nextToken()) - 1;
+			sb.append(dist[start][end]).append("\n");
+		}
+		
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
 	}
 
 }
