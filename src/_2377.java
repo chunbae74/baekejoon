f7cf559f37664164a8d294e8a2df452b28d65b3c
chunbae74 <kaan03@naver.com>
2024-06-03 23:48:06 +0900

    24.6.3

---
+++ src/_2377.java
@@ -0,0 +1,18 @@
+
+public class _2377 {
+
+	public static void main(String[] args) {
+		System.out.println(" _________\r\n"
+				+ " \\_     _/\r\n"
+				+ "   \\   /\r\n"
+				+ "    | |\r\n"
+				+ "   /   \\\r\n"
+				+ "  /     \\\r\n"
+				+ " |       |\r\n"
+				+ "/---------\\\r\n"
+				+ "| \\-/ \\-/ |\r\n"
+				+ "\\---------/\r\n"
+				+ " \\_______/");
+	}
+
+}
---
+++ src/_2420.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class _2420 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n1 = Integer.parseInt(st.nextToken());
+		int n2 = Integer.parseInt(st.nextToken());
+		bw.write(Math.abs(n1 - n2) + "");
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
