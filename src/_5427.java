e5fb6f0d7aa827bbf2d33784d4a1856b712e87e7
chunbae74 <kaan03@naver.com>
2024-08-11 10:45:09 +0900

    24.08.11

--- src/_5427.java
+++ src/_5427.java
@@ -9,6 +9,8 @@ public class _5427 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
 		final int INF = Integer.MAX_VALUE >> 1;
 		final int[] dx = new int[] { 0, 0, 1, -1 };
 		final int[] dy = new int[] { 1, -1, 0, 0 };
@@ -17,21 +19,22 @@ public class _5427 {
 		for (int t = 0; t < T; t++) {
 			Queue<int[]> queue = new LinkedList<>();
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int Y = Integer.parseInt(st.nextToken());
 			int X = Integer.parseInt(st.nextToken());
+			int Y = Integer.parseInt(st.nextToken());
 			/*
 			 * -1 : 벽 (#)
 			 * 0 : 빈공간 (.)
 			 * 1 : 불 (*)
 			 */
 			int[][] graph = new int[Y][X];
-			int[][] dist = new int[Y][X];
+			int[][] distOfFire = new int[Y][X];
+			int[][] distOfPlayer = new int[Y][X];
 			int startX = -1;
 			int startY = -1;
 			for (int y = 0; y < Y; y++) {
 				String l = br.readLine();
 				for (int x = 0; x < X; x++) {
-					dist[y][x] = INF;
+					distOfFire[y][x] = distOfPlayer[y][x] = INF;
 					char c = l.charAt(x);
 					if (c == '#') {
 						graph[y][x] = -1;
@@ -43,26 +46,75 @@ public class _5427 {
 						graph[y][x] = 0;
 					} else if (c == '*') {
 						graph[y][x] = 1;
-						queue.offer(new int[] { x, y });
+						distOfFire[y][x] = 0;
+						queue.offer(new int[] { x, y, distOfFire[y][x] });
 					}
 				}
 			}
 			
-			dist[startY][startX] = 0;
-			queue.offer(new int[] { startX, startY });
 			while (!queue.isEmpty()) {
 				int nowX = queue.peek()[0];
 				int nowY = queue.peek()[1];
+				int nowCost = queue.peek()[2];
 				queue.poll();
+				
+				if (distOfFire[nowY][nowX] < nowCost) continue;
 				
 				for (int i = 0; i < 4; i++) {
 					int nextX = nowX + dx[i];
 					int nextY = nowY + dy[i];
 					
 					if (0 > nextX || nextX >= X || 0 > nextY || nextY >= Y) continue;
+					if (graph[nextY][nextX] == -1) continue;
+					if (distOfFire[nextY][nextX] > nowCost + 1) {
+						distOfFire[nextY][nextX] = nowCost + 1;
+						queue.offer(new int[] { nextX, nextY, distOfFire[nextY][nextX] });
+					}
 				}
 			}
+			
+			queue.clear();
+			distOfPlayer[startY][startX] = 0;
+			queue.offer(new int[] { startX, startY, distOfPlayer[startY][startX] });
+			while (!queue.isEmpty()) {
+				int nowX = queue.peek()[0];
+				int nowY = queue.peek()[1];
+				int nowCost = queue.peek()[2];
+				queue.poll();
+				
+				if (distOfPlayer[nowY][nowX] < nowCost) continue;
+				
+				for (int i = 0; i < 4; i++) {
+					int nextX = nowX + dx[i];
+					int nextY = nowY + dy[i];
+					
+					if (0 > nextX || nextX >= X || 0 > nextY || nextY >= Y) continue;
+					if (graph[nextY][nextX] == -1) continue;
+					
+					if (distOfFire[nextY][nextX] <= nowCost + 1) continue;
+					
+					if (distOfPlayer[nextY][nextX] > nowCost + 1) {
+						distOfPlayer[nextY][nextX] = nowCost + 1;
+						queue.offer(new int[] { nextX, nextY, distOfPlayer[nextY][nextX] });
+					}
+				}
+			}
+			
+			int min = INF;
+			// 상, 하
+			for (int x = 0; x < X; x++) {
+				min = Math.min(min, Math.min(distOfPlayer[0][x], distOfPlayer[Y - 1][x]));
+			}
+			// 좌, 우
+			for (int y = 1; y < Y - 1; y++) {
+				min = Math.min(min, Math.min(distOfPlayer[y][0], distOfPlayer[y][X - 1]));
+			}
+			
+			sb.append(min == INF ? "IMPOSSIBLE" : min + 1).append("\n");
 		}
+		
+		System.out.println(sb.toString());
+		
 	}
 
 }
--- src/_9466.java
+++ src/_9466.java
@@ -1,128 +1,82 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
-/*
- * 24.02.21 시간초과 ;;
- * hasTeam:
- * 0 : 아직 모름
- * 1 : 팀 있음
- * -1 : 팀 없음
- */
 public class _9466 {
+	static int N;
 	static int[] arr;
-	static boolean[] visited;
-	static int[] hasTeam;
-	static int count;
+	static boolean[] hasTeam;
+	static final int INF = Integer.MAX_VALUE >> 1;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
+
 		int T = Integer.parseInt(br.readLine());
-		
-		for (int i = 0; i < T; i++) {
-			int n = Integer.parseInt(br.readLine());
-			arr = new int[n + 1];
-			hasTeam = new int[n + 1];
-			count = 0;
+		for (int t = 0; t < T; t++) {
+			N = Integer.parseInt(br.readLine());
+			arr = new int[N + 1];
+			hasTeam = new boolean[N + 1];
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			
-			for (int j = 1; j <= n; j++) {
+			for (int j = 1; j <= N; j++) {
 				arr[j] = Integer.parseInt(st.nextToken());
 				// 자기 자신을 선택한 경우; 팀으로 인정됨
 				if (arr[j] == j) {
-					hasTeam[j] = 1;
+					hasTeam[j] = true;
 				}
 			}
 			
-			for (int j = 1; j <= n; j++) {
+			for (int j = 1; j <= N; j++) {
 				// 팀여부가 이미 정해진 사람은 스킵
-				if (hasTeam[j] != 0) continue;
-				visited = new boolean[n + 1];
-				dfs(j, n);
+				if (hasTeam[j]) continue;
+				dfs(j);
 			}
 			
-			sb.append(count).append("\n");
+			int count = 0;
+			for (int i = 1; i <= N; i++) {
+				if (hasTeam[i]) continue;
+				count ++;
+			}
+			System.out.println(count);
 		}
 		
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
 	}
 	
-	public static void dfs(int num, int n) {
-		Queue<Integer> queue = new LinkedList<>();
-		queue.offer(arr[num]);
-		visited[num] = true;
-		int[] orderArr = new int[n];
-		int order = 0;
-		orderArr[order ++] = num;
-		
-		while (!queue.isEmpty()) {
-			int nextNum = queue.poll();
-			orderArr[order ++] = nextNum;
+	public static void dfs(int startNum) {
+		Stack<int[]> stack = new Stack<>();
+		int[] dist = new int[N + 1];
+		int[] backTracking = new int[N + 1];
+		Arrays.fill(dist, INF);
+		dist[startNum] = 0;
+		stack.add(new int[] { startNum, dist[startNum] });
+		while (!stack.isEmpty()) {
+			int nowNum = stack.peek()[0];
+			int nowCost = stack.peek()[1];
+			stack.pop();
 			
-			// 내가 고른 상대가 이미 팀이 있을 경우
-			if (hasTeam[nextNum] == 1) {
-				for (int i = 0; i < order - 1; i++) {
-					if (hasTeam[orderArr[i]] == 0) {
-						hasTeam[orderArr[i]] = -1;					
-						count ++;								
-					}
-				}
-				return;
-			}
-			// 내가 고른 상대가 이미 팀이 없음이 확정되었을경우
-			else if (hasTeam[nextNum] == -1) {
-				for (int i = 0; i < order - 1; i++) {
-					if (hasTeam[orderArr[i]] == 0) {
-						hasTeam[orderArr[i]] = -1;					
-						count ++;								
-					}
-				}
-				return;
-			}
+			int nextNum = arr[nowNum];
+			if (hasTeam[nowNum] || hasTeam[nextNum]) break;
 			
-			// 어느곳에서 고리 완성
-			if (visited[nextNum]) {
-				// 고리 시작지점과 종료지점이 다르다면
-				if (nextNum != num) {
-					boolean isFound = false;
-					for (int i = 0; i < order; i++) {
-						// 고리가 연결되는 곳 찾음
-						if (orderArr[i] == nextNum) {
-							isFound = true;
-						}
-						if (isFound) {
-							hasTeam[orderArr[i]] = 1;
-						} else {
-							if (hasTeam[orderArr[i]] == 0) {
-								hasTeam[orderArr[i]] = -1;					
-								count ++;								
-							}
-						}
-					}
-					return;
-				}
-				
-				// 팀을 이룬 인원들 결과처리해주기
-				for (int i = 1; i <= n; i++) {
-					if (visited[i]) {
-						hasTeam[i] = 1;
-					}
-				}
-				return;
-			}
+			backTracking[nextNum] = nowNum;
 			
-			// 아직 팀을 이뤘는지, 실패인지 결과가 안나왔을때
-			visited[nextNum] = true;
-			queue.offer(arr[nextNum]);
+			if (dist[nextNum] > nowCost + 1) {
+				dist[nextNum] = nowCost + 1;
+				stack.add(new int[] { nextNum, dist[nextNum] });	
+			// 이미 최신화가 된 경우, 즉 이미 한 사이클이 만들어 진 경우
+			} else {
+				hasTeam[nextNum] = true;
+				chunbae(nextNum, backTracking[nextNum], backTracking);
+			}
+		}
+	}
+	
+	public static void chunbae(int start, int now, int[] backTracking) {
+		if (start == now) return;
+		else {
+			hasTeam[now] = true;
+			chunbae(start, backTracking[now], backTracking);
 		}
 	}
 
