184382cda37311e53cac74b28f34b9003b314436
코딩하는 춘배 <kaan03@naver.com>
2024-08-26 20:33:27 +0900

    24.08.26

--- src/_1149.java
+++ src/_1149.java
@@ -1,8 +1,7 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /*
@@ -10,47 +9,36 @@ import java.util.StringTokenizer;
  * 24.03.20
  */
 public class _1149 {
-	static Integer[][] dp;
-	static int[][] cost;
-	static int min = Integer.MAX_VALUE;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
+		final int INF = Integer.MAX_VALUE >> 1;
 		int N = Integer.parseInt(br.readLine());
 		
-		cost = new int[N + 1][3];
+		// 빨, 초, 파
+		int[][] dp = new int[N][3];
+		int[][] cost = new int[N][3];
 		
-		for (int i = 1; i <= N; i++) {
+		for (int n= 0; n < N; n++) {
+			Arrays.fill(dp[n], INF);
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			cost[i] = new int[] { Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()) };
-		}
-		
-		for (int i = 0; i < 3; i++) {
-			dp = new Integer[N + 1][2];
-			dp[0] = new Integer[] { cost[0][i], i };
-			recur(N);			
-		}
-		
-		bw.write(min + "");
-		bw.flush();
-		bw.close();
-		br.close();
-	}
-	
-	
-	public static int recur(int N) {
-		if (dp[N] == null) {
-			int[] arr = new int[2];
 			for (int i = 0; i < 3; i++) {
-				int preCost = recur(N - 1);
-				if (preCost == cost[N - 1][i]) continue;
-				
+				cost[n][i] = Integer.parseInt(st.nextToken());
 			}
 		}
 		
-		return 0;
+		br.close();
+		
+		dp[0] = cost[0];
+		for (int n = 1; n < N; n++) {
+			dp[n][0] = cost[n][0] + Math.min(dp[n - 1][1], dp[n - 1][2]);
+			dp[n][1] = cost[n][1] + Math.min(dp[n - 1][0], dp[n - 1][2]);
+			dp[n][2] = cost[n][2] + Math.min(dp[n - 1][0], dp[n - 1][1]);
+		}
+		
+		System.out.println(Math.min(dp[N - 1][0], Math.min(dp[N - 1][1], dp[N - 1][2])));
+		
+		
 	}
-
 }
